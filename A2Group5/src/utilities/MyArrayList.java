61b7cafed0d997ef1ec4287b46f9bb0c364e01a1
Aster Walton <Aster.Walton@edu.sait.ca>
2022-11-04 13:02:23 -0600

    Finished initial version of MyArrayList, still needs bug testing

--- A2Group5/src/utilities/MyArrayList.java
+++ A2Group5/src/utilities/MyArrayList.java
@@ -28,30 +28,48 @@ public class MyArrayList<E> implements  ListADT<E> {
 
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
+		Iterator<E> iterator = new ArrayBasedIterator();
+		int pos = 0;
 		Object temp1 = "";
+		Object temp2 = "";
 		temp1 = array[index];
+		try {
 		array[index] = toAdd;
-		for (i=index+1; i<array.size()-1; i++) { //iterates through list and slides each value over by one after the insertion point
-			Object temp2 = array[index];
+		while (iterator.hasNext()) { //iterates through list and slides each value over by one after the insertion point
+			pos++;
+			while (pos > index || iterator.hasNext()){
+			pos++;
+			temp2 = array[index];
 			array[index] = temp1;
 			temp1 = temp2;
-		return false;
+			}
+		}
+		}
+			catch (NullPointerException e) {
+				System.out.println("Error: Null items cannot be added.");
+				return false;
+		} 	catch (IndexOutOfBoundsException e) {
+			System.out.println("Error: Index out of bounds");
+			return false;
+		
+	}
+		return true;
 	}
 
 	@Override
 	public boolean add(E toAdd) throws NullPointerException {
 		try {
 		if ((array.size() + 1) >= capacity) {
-		array[size]=item;
+		array[size]=toAdd;
 		size++;
 		return true;
 		}
-		else {
+		else { //If the array isn't big enough, we make a new one and copy the first into it
 			Object[] newArray = new Object[array.size() + 1];
 			for (int i = 0; 1 < array.length; i++) {
 				newArray[i] = array[i];
 			}
-			newArray[size++]=item;
+			newArray[size++]=toAdd;
 			array = newArray;
 			return true;
 		}
@@ -82,7 +100,7 @@ public class MyArrayList<E> implements  ListADT<E> {
 			return array[index];
 		}
 		catch (IndexOutOfBoundsException e) {
-			System.out.println("Error: Null list cannot be added.");
+			System.out.println("Error: Index out of bounds");
 		}
 	}
 
@@ -96,18 +114,21 @@ public class MyArrayList<E> implements  ListADT<E> {
 			size--;
 		}
 		catch (IndexOutOfBoundsException e) {
-			
+			System.out.println("Error: Index out of bounds");
 		}
 		return removed;
 	}
 
 	@Override
 	public E remove(E toRemove) throws NullPointerException {
+		Iterator<E> iterator = new ArrayBasedIterator();
 		Object removed;
+		int pos = 0;
 		try {
-			for (int i = 0; i < size-1; i++) {
+			while (iterator.hasNext()) {
+				pos++;
 				if (array[i] == toRemove) {
-					removed = array[i];
+					removed = array[pos];
 					for (int j = i; j < size-1; j++) {//Once a match is located, shift everything over one
 				array[j] = array[j+1];//This just goes through the array, starting at the index, and overwrites each element with the next element
 			}
@@ -115,18 +136,30 @@ public class MyArrayList<E> implements  ListADT<E> {
 			}
 			size--;
 		}
-		catch (IndexOutOfBoundsException e) {
-			
+		catch (NullPointerException e) { 
+			System.out.println("Error: Input value cannot be null");
 		}
 		return removed;
 	}
 
 	@Override
 	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		Iterator<E> iterator = new ArrayBasedIterator();
+		Object removed;
+		int pos = 0;
+		try { 
+			while (iterator.hasNext()) {
+			pos++;
+			if (pos == index) {
+			removed = array[pos];
+			array[pos] = toChange;
+			}
+			}
+		} catch (NullPointerException e) { System.out.println("Error: Input value cannot be null");
+		} catch (IndexOutOfBoundsException e) { System.out.println("Error: Index out of bounds");
+	} return removed;
 	}
-
+			
 	@Override
 	public boolean isEmpty() {
 		boolean isEmpty = (list.size==0);
@@ -135,28 +168,60 @@ public class MyArrayList<E> implements  ListADT<E> {
 
 	@Override
 	public boolean contains(E toFind) throws NullPointerException {
-		// TODO Auto-generated method stub
+		Iterator<E> iterator = array.iterator();
+		int pos = 0;
+		try { 
+			while (iterator.hasNext()) {
+				if (iterator.next().equals("toFind")) {
+					return true;
+			}
+		} 
+		} catch (NullPointerException e) { System.out.println("Error: Input value cannot be null");
 		return false;
+	}
 	}
 
 	@Override
 	public E[] toArray(E[] toHold) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+		Iterator<E> iterator = array.iterator();
+		int size = array.size();
+		int pos = 0;
+		toHold = new Object[size];
+		while (iterator.hasNext()) {
+			toHold[pos] = array[pos];
+			pos++;
+		}
+		return toHold;
+		} catch (NullPointerException e) { System.out.println("Error: Input array cannot be null");
+		return false; 
+		}
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+		Iterator<E> iterator = array.iterator();
+		int size = array.size();
+		int pos = 0;
+		Object[] newArray = new Object[size];
+		while (iterator.hasNext()) {
+			newArray[pos] = array[pos];
+			pos++;
+		}
+		return newArray;
+		} catch (NullPointerException e) { System.out.println("Error: Input array cannot be null");
+		return false; 
+		}
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		Iterator<E> iterator = new ArrayBasedIterator();
+		return iterator;
 	}
 
+	//Should these be implemented? I have them in the class below
 //	@Override
 //	public boolean hasNext() {
 //		// TODO Auto-generated method stub
@@ -173,24 +238,23 @@ private class ArrayBasedIterator implements Iterator<E>{
 
 	//Atributes 
 	
-	private E[] copyeOfElement; 
-	private int pos;  
+	private E[] copyeOfElement = new ArrayBasedIterator(); 
+	private int pos;
 	
 	public ArrayBasedIterator() {
 		copyeOfElement = (E[]) (new Object[size()]);
-		//System.arraycopy(array, 0, copyeOfElement, 0, copyeOfElement.length);
+		System.arraycopy(array, 0, copyeOfElement, 0, copyeOfElement.length);
 	}
 
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+		return (pos < copyeOfElement.length());
 	}
 
 	@Override
 	public E next() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		return (E) copyeOfElement[pos++];
+		
 	}
 	
 }
